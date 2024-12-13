8ab63a8fcd15433bbe6b7b7e236340d076f0b757
maximka1366 <maximka66@mail.ru>
2024-12-13 12:51:46 +0300

    Delete module_18_2 directory

--- module_18_2/UrbanDjango/task2/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-# Create your views here.
-def index_funk(request):
-    return render(request, "second_task/func_templ.html")
-
-class index_class(TemplateView):
-    template_name = "second_task/class_templ.html"
